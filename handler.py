85fd96584f5eeabeadc6d450cde5c467c29983c1
Trickster <mohdzohlof@gmail.com>
2019-01-21 19:54:41 +0200

    remove extra code

--- handler.py
+++ handler.py
@@ -49,7 +49,6 @@ def update_info(settings_error_label, entries, navigation):
 
     if len(org_name) > 0 or len(org_pass) > 0:
         org_info = db.get_org_by_name(org_name)
-        print(org_info)
         if org_name != org_info[1]:
             settings_error_label.configure(text="Organization does not exist!")
         elif org_pass != org_info[2]:
